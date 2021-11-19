1e60b4649dddfb7b13756f37d61f3f4031448065
Victoria <victoriaflowers13@yahoo.com>
2021-11-18 21:39:02 -0600

    Initial commit

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,46 +1,9 @@
 <template>
   <div class="bg-green-500 text-yellow-200">
-    <div class="container mx-auto flex items-center justify-between">
-      <h1 class="tracking-tighter text-3xl font-thin">
-        Puppy<span class="font-normal">Barn</span>
+    <div class="container mx-auto flex items-center justify-center">
+      <h1 class="py-8 tracking-tighter text-3xl font-thin">
+        Dad<span class="font-normal">Jokes</span>
       </h1>
-      <nav>
-        <ul class="flex space-x-4">
-          <router-link to="/">
-            <li
-              class="
-                py-8
-                px-4
-                hover:cursor-pointer hover:bg-green-500 hover:text-green-800
-              "
-            >
-              Home
-            </li>
-          </router-link>
-          <router-link :to="{ name: 'About' }">
-            <li
-              class="
-                py-8
-                px-4
-                hover:cursor-pointer hover:bg-green-500 hover:text-green-800
-              "
-            >
-              About
-            </li>
-          </router-link>
-          <router-link :to="{ path: '/hello', name: 'Hello' }">
-            <li
-              class="
-                py-8
-                px-4
-                hover:cursor-pointer hover:bg-green-500 hover:text-green-800
-              "
-            >
-              Hello
-            </li>
-          </router-link>
-        </ul>
-      </nav>
     </div>
   </div>
 </template>
--- src/components/pages/about.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <h1>About Page</h1>
-  <h1>-----------</h1>
-  <p>We believe in happiness and comfort for all pups in all homes. Therefore, we created a website with all of our fun, exciting, and comforting items we can offer to make every pup  happy and fulfilled in their home. :)</p>
-</template>
